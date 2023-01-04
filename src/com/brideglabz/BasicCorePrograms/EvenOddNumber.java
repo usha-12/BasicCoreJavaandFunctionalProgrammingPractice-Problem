b45f771b3add8648e93ac6873ee26c5fc409a2e2
usha-12 <usha12ahirwar@gmail.com>
2023-01-04 22:22:45 +0530

    Java Program to Check Whether a Number is Even or Odd

---
+++ src/com/brideglabz/BasicCorePrograms/EvenOddNumber.java
@@ -0,0 +1,21 @@
+package com.brideglabz.BasicCorePrograms;
+
+import java.util.Scanner;
+
+public class EvenOddNumber {
+    public static void checkNumberEvenOrOdd(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number :");
+        int number = sc.nextInt();
+        if (number%2 == 0){
+            System.out.println("Number is Even :"+number);
+        }else {
+            System.out.println("Number Is Odd :"+number);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome to Even Odd Number program");
+        checkNumberEvenOrOdd();
+    }
+}
+//8. Java Program to Check Whether a Number is Even or Odd
