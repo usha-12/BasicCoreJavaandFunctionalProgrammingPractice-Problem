0adff9d32155550c307607f7fbf1906a8b1ad673
usha-12 <usha12ahirwar@gmail.com>
2023-01-04 22:19:03 +0530

    Java Program to Compute Quotient and Remainder

---
+++ src/com/brideglabz/BasicCorePrograms/QuotientAndRemainder.java
@@ -0,0 +1,24 @@
+package com.brideglabz.BasicCorePrograms;
+
+import java.util.Scanner;
+
+public class QuotientAndRemainder {
+    public static void checkQuotientRemainder(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Dividend :");
+        int dividend = sc.nextInt();
+        System.out.println("Enter Divisor :");
+        int divisor = sc.nextInt();
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+        System.out.println("Quotient = " +quotient+"\nremainder = "+remainder);
+
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Program Quotient and Remainder");
+        checkQuotientRemainder();
+
+    }
+}
+//6. Java Program to Compute Quotient and Remainder
