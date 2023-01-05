c69cac7a72e4fd2ebce87f0f0cf635c4a78c6fa2
usha-12 <usha12ahirwar@gmail.com>
2023-01-05 23:15:31 +0530

    check whether an alphabet is vowel or consonant

---
+++ src/com/brideglabz/BasicCorePrograms/AlphabetVowelConsonant.java
@@ -0,0 +1,26 @@
+package com.brideglabz.BasicCorePrograms;
+
+import java.util.Scanner;
+
+public class AlphabetVowelConsonant {
+    public static void checkVowelOrConsonant(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Character :");
+        char ch = sc.next().charAt(0);
+        if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+            System.out.println("Vowel :");
+        }
+        else {
+            System.out.println("Consonant :");
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(" Welcome to the program Alphabet Vowel or Consonant ");
+        checkVowelOrConsonant();
+
+    }
+}
+// a e i o u = vowel
+// b c d etc.. = consonant
+// Program to Check Whether an Alphabet is Vowel or Consonant
+
